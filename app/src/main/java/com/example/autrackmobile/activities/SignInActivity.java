a12ef1c03b631cb8b4d6339a174d81b8809b9fbf
jblaze127 <johnperez3@mail.adelphi.edu>
2024-03-06 15:49:49 -0500

    step 2

--- app/src/main/java/com/example/autrackmobile/activities/SignInActivity.java
+++ app/src/main/java/com/example/autrackmobile/activities/SignInActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.autrackmobile.R;
 import com.example.autrackmobile.databinding.ActivitySignInBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -23,5 +27,22 @@ public class SignInActivity extends AppCompatActivity {
     private void setListeners() {
         binding.newAccount.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(), SignUpActivity.class)));
+        binding.signIn.setOnClickListener(v -> addDataToFirestore());
+    }
+
+    private void addDataToFirestore() {
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("first_name", "John");
+        data.put("last_name", "Perez");
+        database.collection("users")
+                .add(data)
+                .addOnSuccessListener(documentReference -> {
+                    Toast.makeText(getApplicationContext(), "Data Inserted", Toast.LENGTH_SHORT).show();
+                })
+                .addOnFailureListener(exception -> {
+                    Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+
     }
 }
